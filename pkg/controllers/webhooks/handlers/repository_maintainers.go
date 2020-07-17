886836f817c09e396fb15cdfa8131d7d9c5e9513
Gerrit <tirreg91@googlemail.com>
2020-07-17 10:06:27 +0200

    Create repo maintainers team.

---
+++ pkg/controllers/webhooks/handlers/repository_maintainers.go
@@ -0,0 +1,45 @@
+package handlers
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	v3 "github.com/google/go-github/v32/github"
+	"github.com/metal-stack/metal-robot/pkg/controllers"
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
+)
+
+var (
+	MaintainersTeamPostfix = "maintainers"
+)
+
+type RepositoryMaintainersParams struct {
+	Logger         *zap.SugaredLogger
+	RepositoryName string
+	CreatorID      int64
+	Client         *v3.Client
+}
+
+func CreateRepositoryMaintainersTeam(ctx context.Context, p *RepositoryMaintainersParams) error {
+	var (
+		name        = fmt.Sprintf("%s-%s", p.RepositoryName, MaintainersTeamPostfix)
+		description = fmt.Sprintf("Maintainers of %s", p.RepositoryName)
+	)
+
+	_, _, err := p.Client.Teams.CreateTeam(ctx, controllers.GithubOrganisation, v3.NewTeam{
+		Name:        name,
+		Description: v3.String(description),
+		Maintainers: []string{strconv.FormatInt(p.CreatorID, 10)},
+		RepoNames:   []string{p.RepositoryName},
+		Privacy:     v3.String("closed"),
+	})
+	if err != nil {
+		return errors.Wrap(err, "error creating maintainers team")
+	}
+
+	p.Logger.Infow("created new maintainers team for repository", "repository", p.RepositoryName, "team", name)
+
+	return nil
+}
