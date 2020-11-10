31684d62e3c8701bedb4d9dff0d2e7911e419449
Gerrit <Gerrit91@users.noreply.github.com>
2020-11-10 17:04:59 +0100

    Increase handle timeout. (#31)

--- pkg/webhooks/constants/constants.go
+++ pkg/webhooks/constants/constants.go
@@ -4,5 +4,7 @@ import "time"
 
 const (
 	// WebhookHandleTimeout is the duration after which a webhook handle function context times out
-	WebhookHandleTimeout = 10 * time.Second
+	// the entire thing is asynchronuous anyway, so the VCS will get an immediate response, this is just
+	// that we do not have processing of events hanging internally
+	WebhookHandleTimeout = 60 * time.Second
 )
