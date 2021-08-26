0e11d2f849a3abc0d1d2364bceaa6b22d4d5b5c6
Gerrit <Gerrit91@users.noreply.github.com>
2021-08-26 14:06:55 +0200

    Dependency updates. (#46)

--- pkg/webhooks/github/actions/release_drafter_test.go
+++ pkg/webhooks/github/actions/release_drafter_test.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/blang/semver"
 	"github.com/google/go-cmp/cmp"
-	v3 "github.com/google/go-github/v37/github"
+	v3 "github.com/google/go-github/v38/github"
 	"go.uber.org/zap/zaptest"
 )
 
