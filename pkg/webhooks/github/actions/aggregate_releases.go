9446fa534355faef96a650f69adb3d134737225c
Gerrit <tirreg91@googlemail.com>
2024-10-22 10:53:46 +0200

    Create release PR for aggregation repositories.

--- pkg/webhooks/github/actions/aggregate_releases.go
+++ pkg/webhooks/github/actions/aggregate_releases.go
@@ -111,6 +111,28 @@ func NewAggregateReleases(logger *slog.Logger, client *clients.Github, rawConfig
 func (r *AggregateReleases) AggregateRelease(ctx context.Context, p *AggregateReleaseParams) error {
 	log := r.logger.With("target-repo", r.repoName, "source-repo", p.RepositoryName, "tag", p.TagName)
 
+	openPR, err := findOpenReleasePR(ctx, r.client.GetV3Client(), r.client.Organization(), r.repoName, r.branch, r.branchBase)
+	if err != nil {
+		return err
+	}
+
+	if openPR == nil {
+		pr, _, err := r.client.GetV3Client().PullRequests.Create(ctx, r.client.Organization(), r.repoName, &v3.NewPullRequest{
+			Title:               v3.String("Next release"),
+			Head:                v3.String(r.branch),
+			Base:                v3.String(r.branchBase),
+			Body:                v3.String(r.pullRequestTitle),
+			MaintainerCanModify: v3.Bool(true),
+		})
+		if err != nil {
+			if !strings.Contains(err.Error(), "A pull request already exists") {
+				return err
+			}
+		} else {
+			log.Info("created pull request", "url", pr.GetURL())
+		}
+	}
+
 	patches, ok := r.patchMap[p.RepositoryName]
 	if !ok {
 		log.Debug("skip applying release actions to aggregation repo, not in list of source repositories")
@@ -119,15 +141,10 @@ func (r *AggregateReleases) AggregateRelease(ctx context.Context, p *AggregateRe
 
 	tag := p.TagName
 	trimmed := strings.TrimPrefix(tag, "v")
-	_, err := semver.NewVersion(trimmed)
+	_, err = semver.NewVersion(trimmed)
 	if err != nil {
 		log.Info("skip applying release actions to aggregation repo because not a valid semver release tag", "error", err)
 		return nil
-	}
-
-	openPR, err := findOpenReleasePR(ctx, r.client.GetV3Client(), r.client.Organization(), r.repoName, r.branch, r.branchBase)
-	if err != nil {
-		return err
 	}
 
 	if openPR != nil {
@@ -202,21 +219,6 @@ func (r *AggregateReleases) AggregateRelease(ctx context.Context, p *AggregateRe
 		log.Info("pushed to aggregate target repo", "branch", r.branch, "hash", hash)
 
 		once.Do(func() { r.lock.Unlock() })
-	}
-
-	pr, _, err := r.client.GetV3Client().PullRequests.Create(ctx, r.client.Organization(), r.repoName, &v3.NewPullRequest{
-		Title:               v3.String("Next release"),
-		Head:                v3.String(r.branch),
-		Base:                v3.String(r.branchBase),
-		Body:                v3.String(r.pullRequestTitle),
-		MaintainerCanModify: v3.Bool(true),
-	})
-	if err != nil {
-		if !strings.Contains(err.Error(), "A pull request already exists") {
-			return err
-		}
-	} else {
-		log.Info("created pull request", "url", pr.GetURL())
 	}
 
 	return nil
